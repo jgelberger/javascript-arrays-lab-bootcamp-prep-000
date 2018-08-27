060bf2d4fe84847832af39dcb0a732ceb1cebf4a
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-08-27 02:06:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ function prependKitten(name) {
 
 function removeLastKitten() {
   var original = kittens;
-  kittens.slice(0, kittens.length-1);
+  var newArray = kittens.slice(0, kittens.length-1);
   return 
 }
